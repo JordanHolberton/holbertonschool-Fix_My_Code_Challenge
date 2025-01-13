474ecef22acf967c2b8ec5eb3b0643ff2a1559c3
JordanHolberton <jordan.chatillon76@laposte.net>
2025-01-13 09:47:42 +0100

    Add input validation for size in print_square.js to ensure it is a positive integer

--- challenge/1-print_square.js
+++ challenge/1-print_square.js
@@ -16,7 +16,7 @@ if (process.argv.length <= 2) {
 
 const size = parseInt(process.argv[2], 10);
 
-if (isNaN(size) || size <= 0) {
+if (!Number.isInteger(size) || size <= 0) {
     process.stderr.write("Invalid size\n");
     process.stderr.write("Usage: ./1-print_square.js <size>\n");
     process.stderr.write("Example: ./1-print_square.js 8\n");
